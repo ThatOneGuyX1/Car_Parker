4b29be2566a321651c49693acd54b04a79490ddd
ThatOneGuyX1 <121983425+ThatOneGuyX1@users.noreply.github.com>
2023-04-05 16:36:14 -0600

    Updated variable names

--- Distance.ino
+++ Distance.ino
@@ -1,15 +1,16 @@
-
+// Defineing Global constant
 const int trigPin = 2;
 const int echoPin = 3;
-
 const int blue = 13;
 const int red =11; 
 const int yellow = 9;
 const int green = 7;
-
+// This determines the units, .393 is to convert to inches.
+const int multiplier =  .393;
 
 long duration;
 int distance;
+
 void setup() {
   pinMode(trigPin, OUTPUT); 
   pinMode(echoPin, INPUT); 
@@ -24,31 +25,11 @@ void loop() {
   RunTrigPin();
 
   duration = pulseIn(echoPin,HIGH);
-  distance = (duration * 0.034 / 2)*.393;
+  distance = (duration * 0.034 / 2)* multiplier; // This converts the time traveled into a distance we can use.
   Serial.print("Distance: ");
   Serial.println(distance);
 
   outputLED();
-
-
-
-
-  /*
-  // Clears the trigPin
-  digitalWrite(trigPin, LOW);
-  delayMicroseconds(2);
-  // Sets the trigPin on HIGH state for 10 micro seconds
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
-  // Reads the echoPin, returns the sound wave travel time in microseconds
-  duration = pulseIn(echoPin, HIGH);
-  // Calculating the distance
-  distance = (duration * 0.034 / 2)*.393;
-  // Prints the distance on the Serial Monitor
-  Serial.print("Distance: ");
-  Serial.println(distance);
-  */
 }
 
 void RunTrigPin()
@@ -61,29 +42,34 @@ void RunTrigPin()
 }
 
 void outputLED()
-{
-  if (distance <= 12 && distance > 9)
+{ /*
+  This block of code is designed to change the LED based off of distance, if the distance is greater than a set max distance now lights will be turned on. 
+  Each LED stage represent a quarter of the distance. The Ideal state is the last quarter. This could be changed based of needs.
+*/
+  int maxDistance = 12;
+
+  if (distance <= maxDistance && distance > maxDistance*.75)
   {
     digitalWrite(green,HIGH);
     digitalWrite(yellow,LOW);
     digitalWrite(red,LOW);
     digitalWrite(blue,LOW);
   }
-  else if (distance <=9 && distance > 4)
+  else if (distance <= maxDistance *.75 && distance > maxDistance * .5)
   {
     digitalWrite(green,LOW);
     digitalWrite(yellow,HIGH);
     digitalWrite(red,LOW);
     digitalWrite(blue,LOW);
   }
-  else if (distance <= 4 && distance > 2)
+  else if (distance <= maxDistance*.5  && distance > maxDistance * .25)
   {
     digitalWrite(green,LOW);
     digitalWrite(yellow,LOW);
     digitalWrite(red,HIGH);
     digitalWrite(blue,LOW);
   }
-  else if (distance <= 2)
+  else if (distance <= maxDistance * .25)
   {
     digitalWrite(green,LOW);
     digitalWrite(yellow,LOW);
